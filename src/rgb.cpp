04018628548e1d6c66c55cbe098f428c597f3871
mwdmwd <pmwpmw@protonmail.ch>
2018-02-08 20:42:58 +0100

    Clamp RGB values to <0;255>

--- src/rgb.cpp
+++ src/rgb.cpp
@@ -8,8 +8,12 @@ uint16_t gammaCorr8to16(uint8_t val)
 	return (uint16_t)(pow((float)val / 255.f, 2.8f) * 1023 + 0.5);
 }
 
+#define CLAMP(x,min,max) do{if((x)>(max))(x)=(max);else if((x)<(min))(x)=(min);}while(0)
 void setRGB(int nr, int ng, int nb)
 {
+	CLAMP(nr, 0, 255);
+	CLAMP(ng, 0, 255);
+	CLAMP(nb, 0, 255);
 	if(r != nr)
 	{
 		analogWrite(RED_PIN, gammaCorr8to16(nr)); r = nr;
